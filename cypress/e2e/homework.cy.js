e2c9d492215b65fde24a53a4a7b60287cfcebe68
OlyaProdaniuk <digitaliz29@gmail.com>
2023-05-17 20:42:30 +0200

    fixed parameters

--- cypress/e2e/homework.cy.js
+++ cypress/e2e/homework.cy.js
@@ -1,16 +1,14 @@
 describe("Multiple tests", () => {
-  const testForm = (testData, expectedResult) =>
+  const testForm = (email, password) =>
     function () {
       cy.visit("https://sanitarskyi-ngx-admin.herokuapp.com/");
       cy.get('[alt="Material Dark Theme"]').click();
       cy.get(".menu-title.ng-tns-c141-9").click();
       cy.get(".menu-title.ng-tns-c141-11").click();
-      cy.get("#inputEmail1")
-        .type(testData)
-        .should("contain.value", `${expectedResult}`);
+      cy.get("#inputEmail1").type(email).should("contain.value", `${email}`);
       cy.get("#inputPassword2")
-        .type(testData)
-        .should("contain.value", `${expectedResult}`);
+        .type(password)
+        .should("contain.value", `${password}`);
       cy.get(".inner-circle").first().click();
       cy.get("[type='submit']").eq(1).should("contain.text", "Sign in").click();
     };
